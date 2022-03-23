47bcbc6f75a713df48bac49e1143eefc12321dd3
fidailll <91362442+fidailll@users.noreply.github.com>
2022-03-24 02:02:06 +0500

    ver 0.1.1

--- lib/stock_symbol.dart
+++ lib/stock_symbol.dart
@@ -8,6 +8,7 @@ class GetRequest {
   late String apiKey;
   late String exchange;
   late String token;
+  List<dynamic> listStock = [];
 
   Future<Dio?> stockSymbol() async {
     exchange = 'US';
@@ -16,74 +17,21 @@ class GetRequest {
     try {
       var options = BaseOptions(
         method: 'GET',
-        // headers: {
-        //   'authorization':
-        //       'Basic ${_loginController.text}-|+${sha256.convert(utf8.encode(_passController.text))}',
-        //  },
-        //headers: {
-        //  'authorization':
-        //       'Basic test-|+-9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08',
-        //  },
         contentType: Headers.jsonContentType,
         responseType: ResponseType.plain,
         baseUrl: url,
-        connectTimeout: 7000,
-        receiveTimeout: 7000,
+        //connectTimeout: 7000,
+        //receiveTimeout: 7000,
       );
 
       Dio dio = Dio(options);
       var response =
           await dio.get('/api/v1/stock/symbol?exchange=$exchange&token=$token');
       debugPrint(response.data);
-      print(response.statusCode);
-
-      List<dynamic> jsonFormate = json.decode(response.data);
-      print(jsonFormate[1]);
-
-      //     setState(() {
-      //       if (response.data != 'false' && response.statusCode == 200) {
-      //         print(response.data);
-      //         _token = response.data.split('+|-');
-
-      //         Get.to(
-      //             BottomMenu(_token, _loginController.text, _passController.text));
-      //         _loginController.text = '';
-      //         _passController.text = '';
-      //       } else if (response.statusCode != 200) {
-      //         showDialog(
-      //             context: context,
-      //             builder: (BuildContext context) {
-      //               return AlertDialog(
-      //                 title: Text(_dictionary.localization()!['notification']),
-      //                 content: Text(
-      //                     "${_dictionary.localization()!['mistake']}: ${response.statusCode}"),
-      //               );
-      //             });
-      //       } else {
-      //         showDialog(
-      //             context: context,
-      //             builder: (BuildContext context) {
-      //               return AlertDialog(
-      //                 title: Text(
-      //                   _dictionary.localization()!['mistake'],
-      //                 ),
-      //                 content: Text(_dictionary
-      //                     .localization()!['youEnteredTheWrongUsernameOrPassword']),
-      //               );
-      //             });
-      //       }
-      //     });
-      //   } on DioError catch (e) {
-      //     showDialog(
-      //         context: context,
-      //         builder: (BuildContext context) {
-      //           return AlertDialog(
-      //             title: Text(_dictionary.localization()!['mistake']),
-      //             content:
-      //                 Text(_dictionary.localization()!['noConnectionToServer']),
-      //           );
-      //         });
-      //   }
+      //   print(response.statusCode);
+      //Map<String, dynamic>.from(response.data);
+      listStock = json.decode(response.data);
+      //print(listStock![1].runtimeType);
     } on DioError catch (e) {
       print(e);
     }
--- lib/stocks_window.dart
+++ lib/stocks_window.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:stocks_app/stock_symbol.dart';
 
@@ -10,9 +14,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Акции',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        fontFamily: 'Exo2',
+        appBarTheme: AppBarTheme(
+          elevation: 0,
+          centerTitle: true,
+          backgroundColor: Colors.yellow[600],
+          shadowColor: Colors.grey[200],
+          titleTextStyle: TextStyle(color: Colors.black, fontSize: 25),
+        ),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.green[50],
+          unselectedItemColor: Colors.green,
+          selectedItemColor: Colors.green[800],
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Акции США'),
     );
   }
 }
@@ -27,20 +44,136 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   GetRequest _getRequest = GetRequest();
+  List<dynamic> listStock = [];
+
+  @override
+  void initState() {
+    _myRequest();
+    super.initState();
+  }
+
+  Future<void> _myRequest() async {
+    _getRequest.stockSymbol();
+
+    setState(() {
+      Timer(Duration(seconds: 15), () {
+        listStock = _getRequest.listStock;
+        print(listStock);
+      });
+    });
+  }
+
+  Map<int, dynamic> list = {};
 
   @override
   Widget build(BuildContext context) {
-    _getRequest.stockSymbol();
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text(widget.title),
+            Container(),
+            Container(
+              margin: EdgeInsets.only(top: 10, bottom: 10),
+              decoration: BoxDecoration(
+                  color: Colors.yellow[400],
+                  borderRadius: BorderRadius.circular(16),
+                  boxShadow: const []),
+              width: MediaQuery.of(context).size.width / 2,
+              child: TextFormField(
+                // controller: _loginController,
+                //cursorColor: Colors.black,
+                //strutStyle: StrutStyle()
+                decoration: InputDecoration(
+                  labelText: 'Поиск',
+                  icon: Icon(CupertinoIcons.search, color: Colors.white70),
+                  labelStyle: const TextStyle(color: Colors.white70),
+                ),
+              ),
+            )
+          ],
         ),
       ),
+      body: Center(
+        child: FutureBuilder(
+            future: Future.delayed(const Duration(seconds: 15)),
+            builder: (context, snapshot) {
+              return _myList();
+              // return Text('');
+            }),
+      ),
     );
+  }
+
+  Widget _myList() {
+    if (listStock.isNotEmpty) {
+      ListView myList = ListView.builder(
+        itemCount: listStock.length,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(4),
+                      boxShadow: const [
+                        BoxShadow(
+                            blurStyle: BlurStyle.normal,
+                            color: Colors.black12,
+                            blurRadius: 3.0,
+                            spreadRadius: 3.0,
+                            offset: Offset.zero),
+                      ]),
+                  margin: EdgeInsets.only(left: 1, right: 1, top: 1, bottom: 1),
+                  child: Column(
+                    children: [
+                      // SizedBox(height: 10),
+                      // Text('${_getRequest.listStock[index]['currency']}'),
+                      SizedBox(height: 10),
+                      Container(
+                        margin: EdgeInsets.only(
+                            left: 5, right: 5, top: 1, bottom: 1),
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                            '${_getRequest.listStock[index]['displaySymbol']}',
+                            style: TextStyle(fontSize: 25)),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(
+                            left: 5, right: 5, top: 1, bottom: 1),
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                            '${_getRequest.listStock[index]['description']}',
+                            style: TextStyle(fontSize: 15)),
+                      )
+
+                      // SizedBox(height: 10),
+                      // Text('${_getRequest.listStock[index]['displaySymbol']}'),
+                      // SizedBox(height: 10),
+                      // Text('${_getRequest.listStock[index]['figi']}'),
+                      // SizedBox(height: 10),
+                      // Text('${_getRequest.listStock[index]['mic']}'),
+                      // SizedBox(height: 10),
+                      // Text('${_getRequest.listStock[index]['isin']}'),
+                      // SizedBox(height: 10),
+                      // Text('${_getRequest.listStock[index]['shareClassFIGI']}'),
+                      // SizedBox(height: 10),
+                      // Text('${_getRequest.listStock[index]['symbol']}'),
+                      // SizedBox(height: 10),
+                      // Text('${_getRequest.listStock[index]['symbol2']}'),
+                      // SizedBox(height: 10),
+                      // Text('${_getRequest.listStock[index]['type']}'),
+                    ],
+                  )),
+            ],
+          );
+        },
+      );
+      return myList;
+    } else {
+      return Text('download');
+    }
   }
 }
